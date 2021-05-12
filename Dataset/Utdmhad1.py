1b270e6166afe866c5e709aae874e30dbb03b21a
Jessica Sena <jessicasenasouza@gmail.com>
2021-05-11 21:38:06 -0300

    UTDMHAD1 added

---
+++ Dataset/Utdmhad1.py
@@ -0,0 +1,48 @@
+from Dataset.Datasets import Dataset
+import numpy as np
+import glob
+import os
+from scipy.io import loadmat
+
+actNameUTDMHAD1 = {
+        1: 'right arm swipe to the left',
+        2: 'right arm swipe to the right',
+        3: 'right hand wave',
+        4: 'two hand front clap',
+        5: 'right arm throw',
+        6: 'cross arms in the chest',
+        7: 'basketball shooting',
+        8: 'draw x',
+        9: 'draw circle clockwise',
+        10: 'draw circle counter clockwise',
+        11: 'draw triangle',
+        12: 'bowling',
+        13: 'front boxing',
+        14: 'baseball swing from right',
+        15: 'tennis forehand swing',
+        16: 'arm curl',
+        17: 'tennis serve',
+        18: 'two hand push',
+        19: 'knock on door',
+        20: 'hand catch',
+        21: 'pick up and throw'
+}
+
+
+class UTDMHAD1(Dataset):
+    def preprocess(self):
+        activities = np.arange(1, 22)
+        pathname = os.path.join(self.dir_dataset, '*.mat')
+        files = glob.glob(pathname=pathname)
+
+        for f in files:
+            act = int(os.path.split(f)[-1].split("_")[0].split("a")[-1])
+            subject = int(os.path.split(f)[-1].split("_")[1].split("s")[-1])
+            trial_id = int(os.path.split(f)[-1].split("_")[2].split("t")[-1])
+            trial = loadmat(f)['d_iner'][0:3]
+            if act in activities:
+                act_name = actNameUTDMHAD1[act]
+                #print('{} {} {}'.format(act_name, subject, trial_id))
+                self.add_info_data(act_name, subject, trial_id, trial, self.dir_save)
+
+        self.save_data(self.dir_save)
