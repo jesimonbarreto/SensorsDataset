c7e0087e30fd502a282e35c9f946fd471a2f92cd
jesimonbarreto <jesimonbarreto@gmail.com>
2021-05-17 00:40:34 -0300

    Add all datasets

--- main.py
+++ main.py
@@ -10,23 +10,8 @@ from Dataset.Mhealth import MHEALTH
 from Dataset.Mhealth import SignalsMHEALTH as sm
 from Dataset.Pamap2 import PAMAP2
 from Dataset.Pamap2 import SignalsPAMAP2 as sp
-
+from Process.Manager import preprocess_datasets
 from Process.Protocol import Loso
-
-def preprocess_datasets(datasets):
-    #devem ser igual
-    num_dim_signals = -1
-    for dtb in datasets:
-        if num_dim_signals == -1:
-            num_dim_signals = len(dtb.get_signals_use())
-        else:
-            if num_dim_signals != len(dtb.get_signals_use()):
-                print('[ERRO] - Todos os datasets que serão unidos devem ter o mesmo número sinais')
-                sys.exit()
-
-    for dtb in datasets:
-        #load and save file preprocess
-        dtb.preprocess()
 
 
 if __name__ == "__main__":
@@ -44,13 +29,14 @@ if __name__ == "__main__":
         file_pm = '/home/jesimon/Documents/others/datasets/PAMAP2_Dataset/Optional/'
         file_mh = '/home/jesimon/Documents/others/datasets/MHEALTHDATASET/'
     
-    #Creating dataset
+    #Creating datasets
     #name, dir_dataset, dir_save, freq = 100, trial_per_file=100000
     w = Wisdm('Wisdm', file_wisdm, dir_datasets, freq = 20, trials_per_file = 1000000)
     utd = UTDMHAD1('UTD1', file_utd1, dir_datasets, freq = 50, trials_per_file = 1000000)
     p2 = PAMAP2('Pamap2', file_pm, dir_datasets, freq = 50, trials_per_file = 10000)
     mh = MHEALTH('Mhealth', file_mh, dir_datasets, freq = 100, trials_per_file = 10000)
 
+    #Define signals of each dataset
     sig_w = [sw.acc_hand_X, sw.acc_hand_Y, sw.acc_hand_Z] 
     w.set_signals_use(sig_w)
 
@@ -62,16 +48,18 @@ if __name__ == "__main__":
     
     sig_m = [sm.acc_chest_X, sm.acc_chest_Y, sm.acc_chest_Z]
     mh.set_signals_use(sig_m)
+    
     #list datasets
     datasets = [mh]
 
-    #preprocess_datasets(datasets)
+    #preprocessing
+    preprocess_datasets(datasets)
     
     #Creating Loso evaluate generating
     generate_ev = Loso(datasets, overlapping = 0.0, time_wd=5)
     #function to save information e data
     #files = glob.glob(dir_datasets+'*.pkl')
-    generate_ev.simple_generate(dir_save_file, new_freq = 50)
+    generate_ev.simple_generate(dir_save_file, new_freq = 100)
     
 
 
